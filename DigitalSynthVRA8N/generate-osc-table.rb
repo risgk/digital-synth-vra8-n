0121e80d8cc5e91ef15e3ca29119eb639ca96db1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2018-03-17 15:31:40 +0900

    Delete unused tables

--- DigitalSynthVRA8N/generate-osc-table.rb
+++ DigitalSynthVRA8N/generate-osc-table.rb
@@ -111,39 +111,6 @@ generate_osc_wave_table_arrays do |last|
   end
 end
 
-generate_osc_wave_table_arrays(true, 2) do |last|
-  generate_osc_wave_table("org3", last, 1.0 / 1.7 / Math.sqrt(3.0), true) do |n, k|
-    if [1, 2, 3].include?(k)
-      Math.sin((2.0 * Math::PI) * ((n + 0.5) /
-        (1 << OSC_WAVE_TABLE_SAMPLES_BITS)) * (k * 2))
-    else
-      0.0
-    end
-  end
-end
-
-generate_osc_wave_table_arrays(true, 2) do |last|
-  generate_osc_wave_table("org4", last, 1.0 / 1.7 / Math.sqrt(4.0), true) do |n, k|
-    if [1, 2, 3, 4].include?(k)
-      Math.sin((2.0 * Math::PI) * ((n + 0.5) /
-        (1 << OSC_WAVE_TABLE_SAMPLES_BITS)) * (k * 2))
-    else
-      0.0
-    end
-  end
-end
-
-generate_osc_wave_table_arrays(true, 8) do |last|
-  generate_osc_wave_table("org9", last, 1.0 / 1.7 / Math.sqrt(9.0), true) do |n, k|
-    if [1, 2, 3, 4, 6, 8, 10, 12, 16].include?(k)
-      Math.sin((2.0 * Math::PI) * ((n + 0.5) /
-        (1 << OSC_WAVE_TABLE_SAMPLES_BITS)) * (k * 2))
-    else
-      0.0
-    end
-  end
-end
-
 def generate_osc_wave_tables_array(name, organ = false, organ_last = 8)
   $file.printf("const uint8_t* g_osc_#{name}_wave_tables[] = {\n  ")
   $osc_harmonics_restriction_table.each_with_index do |freq, idx|
