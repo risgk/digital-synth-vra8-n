a982a16f3d18305c61c3292cccb0962cb01fa3bb
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2018-04-14 12:21:29 +0900

    Add FILTER NG

--- DigitalSynthVRA8N/filter.h
+++ DigitalSynthVRA8N/filter.h
@@ -19,8 +19,10 @@ class Filter {
   static int16_t        m_y_2;
   static uint8_t        m_cutoff_current;
   static uint8_t        m_cutoff;
+  static uint8_t        m_cutoff_velocity;
   static uint8_t        m_mod_amt;
   static uint8_t        m_noise_gen_amt;
+  static uint16_t       m_rnd_temp;
 
   static const uint8_t AUDIO_FRACTION_BITS = 14;
 
@@ -31,10 +33,12 @@ public:
     m_y_1 = 0;
     m_y_2 = 0;
     m_cutoff_current = 127;
+    m_cutoff_velocity = 64;
+    m_rnd_temp = 1;
     set_cutoff(127);
     set_resonance(0);
     set_env_amt(64);
-    set_noise_gen_amt(0);
+    set_noise_gen_amt(127);
     update_current(0);
     update_coefs();
   }
@@ -55,12 +59,17 @@ public:
     m_noise_gen_amt = controller_value;
   }
 
+  INLINE static void note_on(uint8_t cutoff_velocity) {
+    m_cutoff_velocity = cutoff_velocity;
+  }
+
   INLINE static int16_t clock(uint8_t count, int16_t audio_input, uint8_t mod_input) {
     uint8_t count_and_interval = count & (FILTER_CONTROL_INTERVAL - 1);
     if (count_and_interval == 4) {
       update_current(mod_input);
-    } else if (count_and_interval == 6) {
       update_coefs();
+    } else if (count_and_interval == 6) {
+      update_rnd();
     }
 
     int16_t b_2_over_a_0 = m_b_2_over_a_0_low | (m_b_2_over_a_0_high << 8);
@@ -84,16 +93,24 @@ public:
 
 private:
   INLINE static void update_current(uint8_t mod_input) {
-    int16_t cutoff_candidate = m_cutoff;
+    int16_t cutoff_candidate = m_cutoff + static_cast<int8_t>(m_cutoff_velocity - 64);
     cutoff_candidate += high_sbyte(((m_mod_amt - 64) << 1) * mod_input);
-    // TODO: Not to use IOsc
-//    cutoff_candidate += high_sbyte(((m_noise_gen_amt - 64) << 1) * IOsc<0>::get_rnd8());
+//  cutoff_candidate += high_sbyte((mod_input >> 0) * static_cast<int8_t>(get_white_noise_8()));
+    uint8_t cutoff_target;
     if (cutoff_candidate > 127) {
-      m_cutoff_current = 127;
+      cutoff_target = 127;
     } else if (cutoff_candidate < 0) {
-      m_cutoff_current = 0;
+      cutoff_target = 0;
     } else {
-      m_cutoff_current = cutoff_candidate;
+      cutoff_target = cutoff_candidate;
+    }
+
+    if (m_cutoff_current + FILTER_CUTOFF_THROUGH_RATE < cutoff_target) {
+      m_cutoff_current += FILTER_CUTOFF_THROUGH_RATE;
+    } else if (m_cutoff_current > cutoff_target + FILTER_CUTOFF_THROUGH_RATE) {
+      m_cutoff_current -= FILTER_CUTOFF_THROUGH_RATE;
+    } else {
+      m_cutoff_current = cutoff_target;
     }
   }
 
@@ -104,6 +121,16 @@ private:
     m_b_2_over_a_0_high = (four_data >>  8) & 0xFF;
     m_a_1_over_a_0_low  = (four_data >> 16) & 0xFF;
     m_a_1_over_a_0_high = (four_data >> 24) & 0xFF;
+  }
+
+  INLINE static void update_rnd() {
+    m_rnd_temp = m_rnd_temp ^ (m_rnd_temp << 5);
+    m_rnd_temp = m_rnd_temp ^ (m_rnd_temp >> 9);
+    m_rnd_temp = m_rnd_temp ^ (m_rnd_temp << 8);
+  }
+
+  INLINE static uint8_t get_white_noise_8() {
+    return low_byte(m_rnd_temp);
   }
 };
 
@@ -118,5 +145,7 @@ template <uint8_t T> int16_t        Filter<T>::m_y_1;
 template <uint8_t T> int16_t        Filter<T>::m_y_2;
 template <uint8_t T> uint8_t        Filter<T>::m_cutoff_current;
 template <uint8_t T> uint8_t        Filter<T>::m_cutoff;
+template <uint8_t T> uint8_t        Filter<T>::m_cutoff_velocity;
 template <uint8_t T> uint8_t        Filter<T>::m_mod_amt;
 template <uint8_t T> uint8_t        Filter<T>::m_noise_gen_amt;
+template <uint8_t T> uint16_t       Filter<T>::m_rnd_temp;
