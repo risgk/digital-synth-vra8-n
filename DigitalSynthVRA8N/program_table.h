264c043dc3af1c62907e542aa2b65264cb8d7441
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2018-07-17 21:23:53 +0900

    Change PRESET

--- DigitalSynthVRA8N/program_table.h
+++ DigitalSynthVRA8N/program_table.h
@@ -24,7 +24,7 @@ const uint8_t preset_table_AMP_EG_ON    [] = {127, 32 , 127, 127, 127, 127, 127,
 const uint8_t preset_table_LFO_RATE     [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 0  , 16 , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_PITCH_LFO_AMT[] = {63 , 65 , 64 , 65 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_CO_LFO_AMT   [] = {64 , 76 , 52 , 64 , 64 , 64 , 64 , 64 };
+const uint8_t preset_table_CO_LFO_AMT   [] = {64 , 70 , 52 , 64 , 64 , 64 , 64 , 64 };
 
 const uint8_t preset_table_PB_RANGE     [] = {2  , 2  , 2  , 2  , 2  , 2  , 2  , 2  };
 const uint8_t preset_table_CC86         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
