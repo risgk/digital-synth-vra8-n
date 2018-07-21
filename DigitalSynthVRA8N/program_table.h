13824ceac7a8668842987623d9fd28aefef56cd5
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2018-07-21 14:34:24 +0900

    Update PRESETs

--- DigitalSynthVRA8N/program_table.h
+++ DigitalSynthVRA8N/program_table.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // PRESET                                     #0   #1   #2   #3   #4   #5   #6   #7   
-const uint8_t preset_table_FILTER_CUTOFF[] = {80 , 32 , 0  , 127, 127, 127, 127, 127};
+const uint8_t preset_table_FILTER_CUTOFF[] = {79 , 31 , 7  , 127, 127, 127, 127, 127};
 const uint8_t preset_table_FILTER_RESO  [] = {80 , 64 , 127, 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_CUTOFF_EG_AMT[] = {80 , 112, 96,  64 , 64 , 64 , 64 , 64 };
+const uint8_t preset_table_CUTOFF_EG_AMT[] = {82 , 112, 94,  64 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 0  , 0  , 0  , 0  , 0  };
 
 const uint8_t preset_table_OSC2_COARSE  [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
@@ -18,11 +18,11 @@ const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 127, 127, 127, 127,
 
 const uint8_t preset_table_CC28         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CC29         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 127, 0  , 0  , 0  , 0  };
+const uint8_t preset_table_LEGATO       [] = {127, 127, 127, 127, 0  , 0  , 0  , 0  };
 const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 127, 127, 127, 127, 127};
 
 const uint8_t preset_table_LFO_RATE     [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 0  , 16 , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 16 , 16 , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_PITCH_LFO_AMT[] = {63 , 65 , 64 , 65 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_CO_LFO_AMT   [] = {64 , 64 , 52 , 64 , 64 , 64 , 64 , 64 };
 
