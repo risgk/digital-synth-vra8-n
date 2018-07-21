68ca2369780412b3a5fd9c50253fa6eee7701f34
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2018-07-21 12:56:56 +0900

    Change PRESET

--- DigitalSynthVRA8N/program_table.h
+++ DigitalSynthVRA8N/program_table.h
@@ -1,15 +1,15 @@
 #pragma once
 
 // PRESET                                     #0   #1   #2   #3   #4   #5   #6   #7   
-const uint8_t preset_table_FILTER_CUTOFF[] = {80 , 64 , 0  , 127, 127, 127, 127, 127};
+const uint8_t preset_table_FILTER_CUTOFF[] = {80 , 32 , 0  , 127, 127, 127, 127, 127};
 const uint8_t preset_table_FILTER_RESO  [] = {80 , 64 , 127, 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_CUTOFF_EG_AMT[] = {80 , 96 , 96,  64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_EG_DECAY     [] = {64 , 127, 96 , 0  , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_CUTOFF_EG_AMT[] = {80 , 112, 96,  64 , 64 , 64 , 64 , 64 };
+const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 0  , 0  , 0  , 0  , 0  };
 
 const uint8_t preset_table_OSC2_COARSE  [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_OSC2_FINE    [] = {68 , 72 , 66 , 72 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_PORTAMENTO   [] = {0  , 32 , 32 , 32 , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_EG_ATTACK    [] = {0  , 32 , 0  , 0  , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_EG_ATTACK    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
 const uint8_t preset_table_OSC_WAVE     [] = {0  , 0  , 0  , 127, 0  , 0  , 0  , 0  };
 const uint8_t preset_table_OSC2_MIX     [] = {127, 127, 127, 127, 0  , 0  , 0  , 0  };
@@ -19,7 +19,7 @@ const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 127, 127, 127, 127,
 const uint8_t preset_table_CC28         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CC29         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 127, 0  , 0  , 0  , 0  };
-const uint8_t preset_table_AMP_EG_ON    [] = {127, 32 , 127, 127, 127, 127, 127, 127};
+const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 127, 127, 127, 127, 127};
 
 const uint8_t preset_table_LFO_RATE     [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 0  , 16 , 0  , 0  , 0  , 0  };
