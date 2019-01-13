3cd3c43238a0b4ef8e4b6d6c7cec0b186f85f2b4
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2019-01-13 14:00:35 +0900

    Revert "Update PRESETs"
    This reverts commit fb2fe9f3a4b1dce2410df6891aaaf5bba9dfd539.

--- DigitalSynthVRA8N/program_table.h
+++ DigitalSynthVRA8N/program_table.h
@@ -14,11 +14,11 @@ const uint8_t preset_table_PITCH_EG_TGT [] = {127, 0  , 127, 127, 127, 127, 127,
 const uint8_t preset_table_FILTER_CUTOFF[] = {79 , 31 , 7  , 127, 127, 127, 127, 127};
 const uint8_t preset_table_FILTER_RESO  [] = {80 , 64 , 127, 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CUTOFF_EG_AMT[] = {82 , 112, 94,  64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 0  , 127, 127, 127, 127};
+const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 127, 127, 127, 127, 127};
 
 const uint8_t preset_table_EG_ATTACK    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 96 , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 0  , 127, 127, 127, 127};
+const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 0  , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 127, 127, 127, 127, 127};
 const uint8_t preset_table_EG_RELEASE   [] = {127, 127, 127, 127, 127, 127, 127, 127};
 
 const uint8_t preset_table_LFO_WAVE     [] = {0  , 0  , 0  , 127, 0  , 0  , 0  , 0  };
@@ -27,7 +27,7 @@ const uint8_t preset_table_PITCH_LFO_AMT[] = {65 , 65 , 64 , 88 , 65 , 64 , 64 ,
 const uint8_t preset_table_CO_LFO_AMT   [] = {64 , 64 , 52 , 64 , 64 , 64 , 64 , 64 };
 
 const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_LFO_RT_EG_AMT[] = {64 , 64 , 64 , 32 , 64 , 64 , 64 , 64 };
+const uint8_t preset_table_LFO_RT_EG_AMT[] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t preset_table_PITCH_LFO_TGT[] = {0  , 127, 127, 127, 127, 127, 127, 127};
 const uint8_t preset_table_CC15         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
@@ -36,7 +36,7 @@ const uint8_t preset_table_CC86         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  ,
 const uint8_t preset_table_CC106        [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CC107        [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
-const uint8_t preset_table_PORTAMENTO   [] = {0  , 32 , 32 , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_KEY_ASSIGN   [] = {0  , 64 , 0  , 127, 0  , 0  , 0  , 0  };
+const uint8_t preset_table_PORTAMENTO   [] = {0  , 32 , 32 , 32 , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 127, 0  , 0  , 0  , 0  };
+const uint8_t preset_table_KEY_ASSIGN   [] = {0  , 64 , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CC89         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
