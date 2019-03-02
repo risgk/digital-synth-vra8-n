55856bd7ea4b07a377c050cfb3b2ed17ece89174
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2019-03-02 21:57:20 +0900

    Add PRESET "#5 WS"

--- DigitalSynthVRA8N/program_table.h
+++ DigitalSynthVRA8N/program_table.h
@@ -2,28 +2,28 @@
 
 // PRESET                                     #0   #1   #2   #3   #4   #5   #6   #7   
 const uint8_t preset_table_OSC_WAVE     [] = {0  , 0  , 0  , 127, 0  , 0  , 0  , 0  };
-const uint8_t preset_table_OSC2_MIX     [] = {64 , 64 , 64 , 64 , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_OSC2_MIX     [] = {64 , 64 , 64 , 64 , 0  , 64 , 0  , 0  };
 const uint8_t preset_table_OSC2_COARSE  [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_OSC2_FINE    [] = {68 , 72 , 66 , 72 , 68 , 64 , 64 , 64 };
+const uint8_t preset_table_OSC2_FINE    [] = {68 , 72 , 66 , 72 , 68 , 68 , 64 , 64 };
 
 const uint8_t preset_table_SUB_OSC_WAVE [] = {127, 0  , 0  , 0  , 127, 0  , 0  , 0  };
 const uint8_t preset_table_SUB_OSC_LEVEL[] = {127, 127, 127, 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_PITCH_EG_AMT [] = {64 , 62 , 64 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_PITCH_EG_TGT [] = {0  , 127, 0  , 0  , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_PITCH_EG_AMT [] = {64 , 62 , 64 , 64 , 64 , 80 , 64 , 64 };
+const uint8_t preset_table_PITCH_EG_TGT [] = {0  , 127, 0  , 0  , 0  , 127, 0  , 0  };
 
-const uint8_t preset_table_FILTER_CUTOFF[] = {76 , 28 , 0  , 127, 127, 127, 127, 127};
-const uint8_t preset_table_FILTER_RESO  [] = {80 , 64 , 127, 0  , 0  , 0  , 0  , 0  };
+const uint8_t preset_table_FILTER_CUTOFF[] = {76 , 28 , 0  , 127, 127, 112, 127, 127};
+const uint8_t preset_table_FILTER_RESO  [] = {80 , 64 , 127, 0  , 0  , 32 , 0  , 0  };
 const uint8_t preset_table_CUTOFF_EG_AMT[] = {82 , 112, 94,  64 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 0  , 127, 127, 127, 127};
+const uint8_t preset_table_AMP_EG_ON    [] = {127, 0  , 127, 0  , 127, 0  , 127, 127};
 
 const uint8_t preset_table_EG_ATTACK    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 96 , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 0  , 127, 127, 127, 127};
-const uint8_t preset_table_EG_RELEASE   [] = {127, 127, 127, 127, 127, 127, 127, 127};
+const uint8_t preset_table_EG_DECAY     [] = {64 , 96 , 96 , 96 , 0  , 32 , 0  , 0  };
+const uint8_t preset_table_EG_SUSTAIN   [] = {127, 0  , 0  , 0  , 127, 0  , 127, 127};
+const uint8_t preset_table_EG_RELEASE   [] = {127, 127, 127, 127, 127, 0  , 127, 127};
 
-const uint8_t preset_table_LFO_WAVE     [] = {0  , 0  , 0  , 127, 0  , 0  , 0  , 0  };
+const uint8_t preset_table_LFO_WAVE     [] = {0  , 0  , 0  , 127, 0  , 32 , 0  , 0  };
 const uint8_t preset_table_LFO_RATE     [] = {64 , 64 , 64 , 80 , 64 , 64 , 64 , 64 };
-const uint8_t preset_table_PITCH_LFO_AMT[] = {65 , 65 , 64 , 88 , 65 , 64 , 64 , 64 };
+const uint8_t preset_table_PITCH_LFO_AMT[] = {65 , 65 , 64 , 88 , 65 , 65 , 64 , 64 };
 const uint8_t preset_table_CO_LFO_AMT   [] = {64 , 64 , 52 , 64 , 64 , 64 , 64 , 64 };
 
 const uint8_t preset_table_LFO_DEPTH    [] = {16 , 16 , 0  , 0  , 0  , 0  , 0  , 0  };
@@ -36,7 +36,7 @@ const uint8_t preset_table_CC86         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  ,
 const uint8_t preset_table_CC106        [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 const uint8_t preset_table_CC107        [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
-const uint8_t preset_table_PORTAMENTO   [] = {0  , 32 , 32 , 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 0  , 0  , 0  , 0  , 0  };
-const uint8_t preset_table_KEY_ASSIGN   [] = {0  , 64 , 0  , 127, 0  , 0  , 0  , 0  };
+const uint8_t preset_table_PORTAMENTO   [] = {0  , 32 , 32 , 0  , 0  , 32 , 0  , 0  };
+const uint8_t preset_table_LEGATO       [] = {0  , 127, 127, 0  , 0  , 127, 0  , 0  };
+const uint8_t preset_table_KEY_ASSIGN   [] = {0  , 64 , 0  , 127, 0  , 127, 0  , 0  };
 const uint8_t preset_table_CC89         [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
