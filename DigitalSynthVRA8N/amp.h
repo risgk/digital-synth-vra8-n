6b8069951930ad4624e898c858b91fb8178f7280
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2019-03-03 23:04:36 +0900

    Revert "Revert "Improve EXPRESSION (2), Overloaded Now""
    This reverts commit ab6906846e4b6f76c7d372e1313aaacb4a20ca6f.

--- DigitalSynthVRA8N/amp.h
+++ DigitalSynthVRA8N/amp.h
@@ -4,11 +4,25 @@
 
 template <uint8_t T>
 class Amp {
+  static uint8_t m_expression;
+
 public:
   INLINE static void initialize() {
+    m_expression = 252;
+  }
+
+  INLINE static void set_expression(uint8_t controller_value) {
+    m_expression = high_byte((controller_value << 1) * (controller_value << 1));
+  }
+
+  INLINE static void set_volume_exp_amt(uint8_t controller_value) {
+    // TODO
   }
 
   INLINE static int16_t clock(int16_t audio_input, uint8_t gain_control) {
-    return mul_q15_q8(audio_input, gain_control) << 1;
+    uint8_t gain = high_byte(gain_control * m_expression) << 1;
+    return mul_q15_q8(audio_input, gain);
   }
 };
+
+template <uint8_t T> uint8_t  Amp<T>::m_expression;
