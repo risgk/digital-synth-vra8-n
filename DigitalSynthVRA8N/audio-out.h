2a71cb24fbd3963c50e8f5100f7e4a12406b6517
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2019-03-10 18:46:21 +0900

    Improve DEBUG option

--- DigitalSynthVRA8N/audio-out.h
+++ DigitalSynthVRA8N/audio-out.h
@@ -41,14 +41,13 @@ public:
       if ((cnt < 64) && (cnt > s_maxCnt)) {
         s_maxCnt = cnt;
       }
-      cnt = s_maxCnt + 1;
+      cnt = s_maxCnt;
 #elif 1
       if (cnt >= 64) {
         cnt = 255;   // Not Over
       }
-      cnt++;
 #endif
-      UDR0 = cnt;
+      UDR0 = cnt++;
       m_count = 0;
     }
 #endif
